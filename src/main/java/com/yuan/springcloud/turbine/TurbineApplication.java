6811710d4074b2ff99902b017565d8abbb992d6c
yuan <zidanepirlo@163.com>
2017-12-16 22:12:28 +0800

    project init construct

---
+++ src/main/java/com/yuan/springcloud/turbine/TurbineApplication.java
@@ -0,0 +1,23 @@
+package com.yuan.springcloud.turbine;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+//@EnableHystrix
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+}
